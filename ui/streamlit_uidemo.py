0a892151d34c86eedb039843266e0b210cdd65e7
Mohit <mohit.turabit@gmail.com>
2026-02-13 12:02:35 +0530

    feat(ui): added fastapi helper functions and using those populated the fields of department, document and generation history in streamlit

--- ui/streamlit_uidemo.py
+++ ui/streamlit_uidemo.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests 
 
 
-FASTAPI_URL= "https://127.0.0.1:8000"
+FASTAPI_URL= "http://127.0.0.1:8000"
 
 st.set_page_config(layout="wide")
 
@@ -38,11 +38,70 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+
+##API helpers to get each and every endpoint
+
+@st.cache_data(ttl=300) #ensures safety against mutations by creating a new copy of data (cached data) and holds data for 300 seconds
+def get_departments_from_fastapi():
+    """To GET the departments from the FASTAPI"""
+    try:
+        response_received = requests.get(f"{FASTAPI_URL}/departments", timeout=10)
+        response_received.raise_for_status() # to check the http response status code
+        return response_received.json().get("departments", [])
+    except Exception as error:
+        st.error(f"Failed to load Departments: {error}")
+        return []
+
+@st.cache_data(ttl=300)
+def get_document_types_from_fastapi(department_name):
+    """To GET the document types from the FASTAPI"""
+    try:
+        response_received = requests.get(f"{FASTAPI_URL}/document-types",params={"department": department_name}, timeout=10)
+        response_received.raise_for_status()
+        return response_received.json().get("document_types", [])
+    except Exception as error:
+        st.error(f"Failed to load Document types: {error}")
+        return []
+    
+@st.cache_data(ttl=300)
+def get_questions_from_fastapi(document_type):
+    """To GET the questions for all the document type from the FASTAPI"""
+    try:
+        response_received = requests.get(f"{FASTAPI_URL}/questions",params={"document_type": document_type}, timeout=10)
+        response_received.raise_for_status() 
+        return response_received.json().get("questions", [])
+    except Exception as error:
+        st.error(f"Failed to load Questions: {error}")
+        return []
+
+@st.cache_data(ttl=600) # holds data for 600 seconds
+def get_notionpage_urls_from_fastapi():
+    """To GET all the generated pages url in notion pages from the FASTAPI"""
+    try: 
+        print("about to call api")
+        response_received = requests.get(f"{FASTAPI_URL}/get_all_urls", timeout=30)
+        response_received.raise_for_status()
+        return response_received.json().get("pages")
+    except Exception as error:
+        st.error(f"Failed to load URLs from Notion pages: {error}")
+        return []
+    
+
+
+#---------------------------------------------------
+# Load the initial data from thee functions
+#---------------------------------------------------
+
+pages = get_notionpage_urls_from_fastapi()
+departments = get_departments_from_fastapi()                           
+department_names = [d["name"] for d in departments]  
+
+
 # -------------------------------------------------
 # Session State (this will get updated based on the documents which are published into notion as well as we will cache those documents so that there is no need to call the document again and again and waste api limits)
 # -------------------------------------------------
 if "history" not in st.session_state:
-    st.session_state.history = ["Doc1", "Doc2", "Doc3", "Doc4", "Doc5", "Doc6"]
+    st.session_state.history = pages
 
 if "answers" not in st.session_state:
     st.session_state.answers = {
@@ -83,16 +142,20 @@ with st.sidebar:
     # ====================================================
     # This will also be generated based on the values stored in mongodb and will load things realtime
     # ====================================================
-    st.selectbox(
+    selected_department = st.selectbox(
         "Department",
-        ["Engineering", "HR", "Finance"],
-        label_visibility="collapsed",
+        department_names or ["(no departments found)"],
+        label_visibility="collapsed"
     )
 
     st.subheader("Document")
+    valid_dept = selected_department and selected_department != "(no departments found)"
+    doc_types = get_document_types_from_fastapi(selected_department) if valid_dept else []
+    document_names = [d["document_type"] for d in doc_types] #here document_type is the fastapi endpoint parameter
+
     st.selectbox(
         "Document",
-        ["Spec", "Policy", "Report"],
+        document_names or ["(no departments found)"],
         label_visibility="collapsed",
     )
 
@@ -101,8 +164,7 @@ with st.sidebar:
     history_container = st.container(height=350)
     with history_container:
         for h in st.session_state.history:
-            st.button(h, use_container_width=True)
-
+            st.markdown(f"<a href='{h.get('url','#')}'>{h.get('title','Untitled')}</a>", unsafe_allow_html=True)
 
 # =================================================
 # MAIN AREA
